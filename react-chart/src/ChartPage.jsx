8715f1f2b3bf324f081fee17fbfa0c599772ca22
JaxChoong <choongjax@gmail.com>
2024-12-02 22:17:13 +0800

    style: made graph the same languages

--- react-chart/src/ChartPage.jsx
+++ react-chart/src/ChartPage.jsx
@@ -4,22 +4,11 @@ import ChartComponent from './chartComponent';
 import './App.css';
 
 function ChartPage() {
-  const [count, setCount] = useState(() => {
-    const savedCount = localStorage.getItem('saveCount');
-    return savedCount ? Number(savedCount) : 0;
-  });
-
   const languageTime = [
     { language: 'javascript', time: 2 },
     { language: 'python', time: 3 },
     { language: 'java', time: 6 },
-    { language: 'c#', time: 1 },
     { language: 'c++', time: 5 },
-    { language: 'go', time: 1 },
-    { language: 'ruby', time: 3 },
-    { language: 'swift', time: 2 },
-    { language: 'kotlin', time: 2 },
-    { language: 'typescript', time: 0 },
   ];
   useEffect(() => {
     localStorage.setItem('languageTime', JSON.stringify(languageTime));
@@ -33,7 +22,7 @@ function ChartPage() {
           Back
         </button>
       </div>
-      <div style={{width:'70%',justifyContent:'center',alignContent:'center',display:'flex',margin:'80px auto'}}>
+      <div style={{width:'60%',justifyContent:'center',alignContent:'center',display:'flex',margin:'100px auto'}}>
         <ChartComponent />
       </div>
     </div>
